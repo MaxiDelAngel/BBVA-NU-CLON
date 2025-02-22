364181bcf268e891c55a8b41b636ae003f8c964d
Maximiliano Del Angel <maxdelangel13@hotmail.com>
2025-02-21 18:15:09 -0600

    Cambios en el diseño de MainScreem.kt

--- app/src/main/java/project/bancorym/MainScreem.kt
+++ app/src/main/java/project/bancorym/MainScreem.kt
@@ -1,25 +1,209 @@
 package project.bancorym
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun MainScreen(){
+fun MainScreen() {
     Column(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Bienvenido Banco RYM", fontSize = 20.sp)
+        //Primera parte del diseño
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(400.dp)
+                .background(Color.LightGray)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.fondologin),
+                contentDescription = "Fondo",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .drawWithContent {
+                        drawContent()
+                        drawRect(
+                            color = Color(0xFF111D79),
+                            alpha = 0.8f
+                        )
+                    },
+                contentScale = ContentScale.Crop
+            )
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Top
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(80.dp)
+                        //.background(Color.Red)
+                        .padding(10.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(text="RYM", color = Color.White, fontWeight = FontWeight.Bold, fontSize = 25.sp)
+                }
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(230.dp)
+                        //.background(Color.Green)
+                        .padding(10.dp)
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Column(
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Text(
+                                text = "Bienvenido Usuario",
+                                color = Color.White,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 25.sp,
+                            )
+                            Spacer(modifier = Modifier.size(10.dp))
+                            Text(
+                                text = "Cambiar de usuario",
+                                color = Color(0xFF2196F3),
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 18.sp,
+                            )
+                            Spacer(modifier = Modifier.size(30.dp))
+                            Button(
+                                onClick = {},
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(50.dp)
+                                    .shadow(5.dp, shape = RectangleShape),
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = Color.White,
+                                    contentColor = Color(0xFF2196F3)
+                                ),
+                                shape = RectangleShape
+                            ) {
+                                Text(
+                                    text = "Acceder a tu cuenta",
+                                    textAlign = TextAlign.Center,
+                                    fontSize = 15.sp,
+                                    modifier = Modifier.fillMaxWidth()
+                                )
+                            }
+                        }
+                    }
+                    Icon(
+                        imageVector = Icons.Default.AccountCircle,
+                        contentDescription = "icono",
+                        tint = Color(0xFF2196F3),
+                        modifier = Modifier
+                            .size(80.dp)
+                            .align(Alignment.TopEnd)
+                            .padding(10.dp, top=30.dp)
+                    )
+                }
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(90.dp)
+                        //.background(Color.Black)
+                        .padding(5.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxHeight(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.AccountCircle,
+                            contentDescription = "icono",
+                            tint = Color(0xFF2196F3),
+                            modifier = Modifier
+                                .size(60.dp)
+                        )
+                        Text(
+                            text = "Token Movil",
+                            color = Color.White,
+                            textAlign = TextAlign.Center,
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Column(
+                        modifier = Modifier.fillMaxHeight(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.AccountCircle,
+                            contentDescription = "icono",
+                            tint = Color(0xFF2196F3),
+                            modifier = Modifier
+                                .size(60.dp)
+                        )
+                        Text(
+                            text = "Operación QR",
+                            color = Color.White,
+                            textAlign = TextAlign.Center,
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Column(
+                        modifier = Modifier.fillMaxHeight(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.AccountCircle,
+                            contentDescription = "icono",
+                            tint = Color(0xFF2196F3),
+                            modifier = Modifier
+                                .size(60.dp)
+                        )
+                        Text(
+                            text = "Emergencia",
+                            color = Color.White,
+                            textAlign = TextAlign.Center,
+                        )
+                    }
+                }
+            }
+        } //Fin de la primera parte del diseño
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
--- app/src/main/java/project/bancorym/SplashScreen.kt
+++ app/src/main/java/project/bancorym/SplashScreen.kt
@@ -1,24 +1,19 @@
 package project.bancorym
 
-import android.window.SplashScreen
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import kotlinx.coroutines.delay
 import project.bancorym.navigation.AppScreens
@@ -39,9 +34,8 @@ fun Splash() {
         modifier = Modifier
             .fillMaxSize()
     ) {
-        // Imagen de fondo
         Image(
-            painter = painterResource(id = R.drawable.fondopantallasplash),
+            painter = painterResource(id = R.drawable.fondosplash),
             contentDescription = "Fondo",
             modifier = Modifier.fillMaxSize(),
             contentScale = ContentScale.Crop
